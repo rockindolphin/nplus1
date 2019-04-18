b52bd742521f9499b3ecaff901859e30fb58325f
Unknown <rockindolphing@gmail.com>
2019-04-18 14:28:33 +0300

    page background

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
+import 'core-js';
 import Vue from 'vue';
+import ScrollMagic from 'scrollmagic';
 import nplus1Components from './components/icons.js';
 
 //set path for webpack chunks
@@ -43,5 +45,74 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		components: nplus1Components			
 	});
 	window.app = app;
+
+	function initCssVariables(){
+		//ширина скроллбара
+		let scrollMeasure = document.createElement('div');
+		scrollMeasure.classList.add('scroll__measure');
+		document.body.appendChild(scrollMeasure);
+		var scrollbarWidth = scrollMeasure.offsetWidth - scrollMeasure.clientWidth;
+		document.querySelector(':root').style.setProperty('--scrollbar-width', scrollbarWidth+'px');
+	}
+	initCssVariables();
+
+	//фоновые картинки
+	function initBackgroundImages(){
+
+		let getCurrentSrc = function(element, cb){
+			var getSrc;
+			if(!window.HTMLPictureElement){
+				if(window.respimage){
+					respimage({elements: [element]});
+				} else if(window.picturefill){
+					picturefill({elements: [element]});
+				}
+				cb(element, element.src);
+				return;
+			}
+
+			getSrc = function(){
+				element.removeEventListener('load', getSrc);
+				element.removeEventListener('error', getSrc);
+				cb(element, element.currentSrc);
+			};
+
+			element.addEventListener('load', getSrc);
+			element.addEventListener('error', getSrc);
+			if(element.complete){
+				getSrc();
+			}
+		}	
+
+		let bg_images = [...document.querySelectorAll('img[data-bg=true]')];
+		bg_images.map((image)=>{
+			let src = image.getAttribute('src');
+			let parent = image.parentNode;
+			if( parent.nodeName.toUpperCase() === 'PICTURE' ){
+				getCurrentSrc(parent.querySelector('img'), (elem, src)=>{
+					parent.parentNode.style.backgroundImage = `url(${src})`;
+				})
+			}else{
+				parent.style.backgroundImage = `url(${src})`;
+			}
+			image.style.display = 'none';
+		});
+	}
+	initBackgroundImages();	
+
+	function initPageBackground(){
+		let controller = new ScrollMagic.Controller(),
+			background = document.querySelector('.page__bg--background'),
+			foreground = document.querySelector('.page__bg--foreground');
+		let scene = new ScrollMagic.Scene({
+						duration: document.body.offsetHeight - window.innerHeight 
+					})
+					.on('progress', function(evt){
+						foreground.style.opacity = evt.progress;
+						document.querySelector(':root').style.setProperty('--page-progress', evt.progress);
+					})
+					.addTo(controller);	
+	}
+	initPageBackground()
 
 });
